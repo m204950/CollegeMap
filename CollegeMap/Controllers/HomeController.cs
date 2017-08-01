f90693205b4050956bd7544f90d63882ef5a4fbf
Tom Corcoran <tjc.corcoran@gmail.com>
2017-08-01 10:43:13 -0500

    Eveything working except for mapping

--- CollegeMap/Controllers/HomeController.cs
+++ CollegeMap/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CollegeMap.Data;
 using Microsoft.EntityFrameworkCore;
+using CollegeMap.Models.CollegeMapViewModels;
+using CollegeMap.Models.CollegeMapModels;
 
 namespace CollegeMap.Controllers
 {
@@ -19,9 +21,77 @@ namespace CollegeMap.Controllers
 
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Colleges.Include(c => c.Type).Include(c => c.HighestDegreeOffered).ToListAsync());
+            /* query components
+             * text that is contained in College Name
+             * minimum degree offered - so must be <= highest degree offered
+             * equals selected type
+             * minimum enrollment context.Persons.Max(p => p.Age)
+             * maximum enrollment
+             * maximum tuition + R&B
+            */
+            IEnumerable<CollegeType> collegeTypes = _context.CollegeTypes.ToList();
+            IEnumerable<DegreeType> degreeTypes = _context.DegreeTypes.ToList();
+            QueryCollegeViewModel queryCollegeViewModel = new QueryCollegeViewModel(collegeTypes, degreeTypes);
+            // set default minimum enrollment to minimum value in DB
+            queryCollegeViewModel.MinimumEnrollment = _context.Colleges.Min(c => c.Enrollment);
+            // set default maximum enrollment to minimum value in DB
+            queryCollegeViewModel.MaximumEnrollment = _context.Colleges.Max(c => c.Enrollment);
+            queryCollegeViewModel.MaxTotalCost = _context.Colleges.Max(c => c.AnnualTuition + c.AnnualRoomAndBoard);
+            return View(queryCollegeViewModel);
         }
 
+        // POST: /Query
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Query(QueryCollegeViewModel queryCollegeViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                int minEnrollment = queryCollegeViewModel.MinimumEnrollment;
+                int maxEnrollment = queryCollegeViewModel.MaximumEnrollment;
+                int maxTotalCost = queryCollegeViewModel.MaxTotalCost;
+                int desiredDegreeLevelID = queryCollegeViewModel.DegreeTypeID;
+                int collegeTypeID = queryCollegeViewModel.CollegeTypeID;
+                string nameContains;
+                if (queryCollegeViewModel.NameContains == null)
+                {
+                    nameContains = "";
+                }
+                else
+                {
+                    nameContains = queryCollegeViewModel.NameContains;
+                }
+                if (collegeTypeID == 500)  // ID for selecting all college types
+                {
+                    queryCollegeViewModel.Colleges = await _context.Colleges.
+                        Where(c => c.Enrollment <= maxEnrollment).Where(c => c.Enrollment >= minEnrollment).
+                        Where(c => c.AnnualTuition + c.AnnualRoomAndBoard <= maxTotalCost).
+                        Where(c => c.Name.ToUpper().Contains(nameContains.ToUpper())).
+                        // following relies on degree types having ascending IDs corresponding to their level
+                        Where(c => c.HighestDegreeOffered.ID >= desiredDegreeLevelID).
+                        Include(c => c.Type).Include(c => c.HighestDegreeOffered).ToListAsync();
+                }
+                else
+                {
+                    queryCollegeViewModel.Colleges = await _context.Colleges.
+                        Where(c => c.Enrollment <= maxEnrollment).Where(c => c.Enrollment >= minEnrollment).
+                        Where(c => c.AnnualTuition + c.AnnualRoomAndBoard <= maxTotalCost).
+                        Where(c => c.Name.ToUpper().Contains(nameContains.ToUpper())).
+                        // following relies on degree types having ascending IDs corresponding to their level
+                        Where(c => c.HighestDegreeOffered.ID >= desiredDegreeLevelID).
+                        Where(c => c.Type.ID == collegeTypeID).
+                        Include(c => c.Type).Include(c => c.HighestDegreeOffered).ToListAsync();
+                }
+
+            }
+            IEnumerable<CollegeType> collegeTypes = _context.CollegeTypes.ToList();
+            IEnumerable<DegreeType> degreeTypes = _context.DegreeTypes.ToList();
+            queryCollegeViewModel.CreateCollegeTypeSelects(collegeTypes);
+            queryCollegeViewModel.CreateDegreeTypeSelects(degreeTypes);
+            return View("Index", queryCollegeViewModel);
+        }
         public IActionResult About()
         {
             if (User.Identity.IsAuthenticated)
---
+++ CollegeMap/Models/CollegeMapViewModels/QueryCollegeViewModel.cs
@@ -0,0 +1,90 @@
+﻿using CollegeMap.Models.CollegeMapModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeMap.Models.CollegeMapViewModels
+{
+    public class QueryCollegeViewModel
+    {
+        [Required]
+        [Display(Name = "Min Enrollment")]
+        [Range(300, 100000)]
+        public int MinimumEnrollment { get; set; }
+
+        [Display(Name = "College Name Contains")]
+        public string NameContains { get; set; }
+
+        [Required]
+        [Display(Name = "Max Enrollment")]
+        [Range(300, 100000)]
+        public int MaximumEnrollment { get; set; }
+
+        [Required]
+        [Display(Name = "Max Tuition plus R&B")]
+        [Range(300, 1000000)]
+        public int MaxTotalCost { get; set; }
+
+        [Required]
+        [Display(Name = "College Type Desired")]
+        public int CollegeTypeID { get; set; }
+
+        [Required]
+        [Display(Name = "Highest Degree Level Neede")]
+        public int DegreeTypeID { get; set; }
+
+        public List<College> Colleges { get; set; }
+
+        public List<SelectListItem> CollegeTypes { get; set; }
+
+        public List<SelectListItem> DegreeTypes { get; set; }
+
+        public void CreateDegreeTypeSelects(IEnumerable<DegreeType> degreeTypes)
+        {
+            DegreeTypes = new List<SelectListItem>();
+
+            foreach (DegreeType degreeType in degreeTypes)
+            {
+                DegreeTypes.Add(new SelectListItem
+                {
+                    Value = degreeType.ID.ToString(),
+                    Text = degreeType.Name
+                });
+            }
+
+        }
+        public void CreateCollegeTypeSelects(IEnumerable<CollegeType> collegeTypes)
+        {
+            CollegeTypes = new List<SelectListItem>();
+
+            foreach (CollegeType collegeType in collegeTypes)
+            {
+                CollegeTypes.Add(new SelectListItem
+                {
+                    Value = collegeType.ID.ToString(),
+                    Text = collegeType.Name,
+                    Selected = true
+                });
+            }
+            CollegeTypes.Add(new SelectListItem
+            {
+                Value = "500",
+                Text = "All"
+            });
+
+        }
+        public QueryCollegeViewModel()
+        {
+        }
+
+        public QueryCollegeViewModel(IEnumerable<CollegeType> collegeTypes, IEnumerable<DegreeType> degreeTypes)
+        {
+
+            CreateCollegeTypeSelects(collegeTypes);
+            CreateDegreeTypeSelects(degreeTypes);
+        }
+    }
+}
